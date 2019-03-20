fc546a026e33f088d3466d9d1b564ac2d3708a63
marcusp619 <marcusp619@gmail.com>
2019-03-20 16:24:19 -0600

    Add schema

---
+++ src/schema.js
@@ -0,0 +1,61 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # The "Query" type is the root of all GraphQL queries.
+  type Query {
+    contract: Contract!
+    invoices: [Invoice]!
+    agreement: Agreement!
+    order: Order!
+    sites: [Site]
+  }
+
+  # "ContractType"
+  type Contract {
+    id: ID
+    service: String
+    term_length: Int
+  }
+
+  # "InvoiceType"
+  type Invoice {
+    id: ID
+    paid: Boolean
+    paid_date: String
+    amount: Float
+    due_date: String
+  }
+
+  # "AgreementType"
+  type Agreement {
+    id: ID
+    project_summary: String
+    scope_of_work: String
+    term_length: Int
+    hourly_rate: Float
+  }
+
+  # "OrderType"
+  type Order {
+    id: ID
+    non_recurring_cost: Float
+    non_recurring_cost_description: String
+    recurring_cost: Float
+    recurring_cost_description: String
+    term_length: Int
+  }
+
+  # "SiteType"
+  type Site {
+    id: ID
+    project_summary: String
+    scope_of_work: String
+    term_length: Int
+    hourly_rate: Float
+    contract_id: ID
+  }
+`;
+
+module.exports = typeDefs;
